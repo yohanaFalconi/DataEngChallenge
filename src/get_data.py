d21338e2efbc6f12136c62ec6f17c1cd320337e2
yfalconi <yohanafalconivivas@gmail.com>
2025-03-09 18:22:54 -0500

    Configured CSV dataset hosting via BigQuery (GCP)

---
+++ src/get_data.py
@@ -0,0 +1,23 @@
+import os
+import pandas as pd
+from google.cloud import bigquery
+
+
+# Inicialización
+client = bigquery.Client.from_service_account_json(r".\src\bigquery-credencial.json")
+
+# Project and dataset
+project_id = "airy-boulevard-453221-c6"
+dataset_id = "dataset_jobs"
+
+# Load data
+def load_bq_table(table_name):
+    sql = f"SELECT * FROM `{project_id}.{dataset_id}.{table_name}`"
+    return pd.DataFrame([dict(row) for row in client.query(sql).result()])
+
+# df_hired = load_bq_table("departments_")
+# df_hired = load_bq_table("jobs_")
+df_hired = load_bq_table("hired_employees_")
+
+print(df_hired)
+
